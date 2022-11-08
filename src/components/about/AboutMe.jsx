ad9e730f4be32220a2d34064ff6e4e55957a5d5a
Lao <lautaro.s.larragueta@gmail.com>
2022-11-08 14:32:08 +0100

    culminado

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -10,15 +10,9 @@ const AboutMe = () => {
       <div className='about-desc' >
         <h3 className='subtitulo'>Algo sobre mi</h3>
         <p className='sobre-mi'>
-            Soy una persona autodidacta de mente abierta con excelente disposición para el Trabajo en equipo,
-            con una buena actitud de resiliencia, compromiso, responsabilidad, paciencia y creatividad.
-            Soy de comunicación clara con la facilidad de poder transmitirme
-            de forma transparente y sincera.
-            Me encanta aprender mediante el conocimiento en todos sus
-            formatos y sentidos.
-            Busco aplicar los conocimientos adquiridos en mis capacitaciones de fullstack developer y
-            especializarme principalmente como front end developer.
-            Quiero hacer parte de proyectos con clientes reales que me permitan mejorar mi pensamiento crítico, adaptabilidad y creatividad con el compromiso de realizar un trabajo eficiente para fortalecer mi aprendizaje laboral y así llevar a cabo lo que el trabajo asignado requiera. 
+            Hola, mi nombre es Lautaro, pero mis amigxs me dicen 
+            Lao tengo 27 años, vivo actualmente en Buenos aires en la localidad de Florencio Varela. Soy una persona autodidacta de mente abierta con excelente disposición para el trabajo en equipo, con buenas habilidades de resiliencia, compromiso, responsabilidad, paciencia y creatividad. Soy de comunicación clara con la facilidad de poder transmitirme de forma transparente y sincera. Me encanta aprender mediante el conocimiento en todos sus formatos y sentidos. Me especializo en front end en estos momentos, pero tengo conocimientos de back end con JavaScript.
+            Y estoy en constante aprendizaje porque pienso a la tecnología como una materia volátil y de constante cambio.
               <br />
                   Tengo conocimientos en las siguientes herramientas:
               <br />
